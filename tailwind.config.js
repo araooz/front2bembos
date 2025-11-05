efb8ecbb50a7126432eb4046c66db9a3933c2347
falo <faliaraoz@gmail.com>
2025-11-04 19:13:28 -0500

    color bembos añadido a .\tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        bembos: "#004AAD",
+      },
+    },
   },
   plugins: [],
 }
